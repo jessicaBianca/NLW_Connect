062ed502e8a16d01c1f857f38cdba663449e40e0
Jessica <jessica.britto@outlook.com>
2025-02-17 21:00:16 -0300

    [Jessica Oliveira] Add funçoes do js e função com um evento

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
-const mensagem = (nome) => {
-    return alert("Seja bem-vindo(a)"+nome)
+/*const mensagem = (nome) => {
+    return alert("Seja bem-vindo(a) "+nome)
+}*/
+
+//mensagem("Jessica")
+
+const app = document.getElementById("app")
+
+const formAction = ()=>{
+    const form = document.getElementById("form")
+    
+    //quando acontece um evento ele bloqueia o submit que é o modo default
+    form.onsubmit = (event)=>{
+
+        //evita o padrão de submit
+        event.preventDefault()
+        alert("Bloqueio do evento")
+    }
 }
 
-mensagem("Jessica")
+
+const startApp = ()=>{
+
+    const content =
+        `<form id="form">
+        <input type="email" name="email" id="email" placeholder="email@email.com"/>
+        <input type="text" name="phone" id="email" placeholder="(xx) 90000-0000"/>
+        <button id="btn_confirmar">Confirmar</button>
+        </form>`
+
+    app.innerHTML = content    
+    formAction()
+}
+
+startApp()
