7966c8ad3190dfc8279ed427d949bb05e9a777f8
Jessica <jessica.britto@outlook.com>
2025-02-19 21:03:49 -0300

    [Jessica Oliveira] Finalização do NLW - Rocketseat

--- Aula 02 - Criando Lógica de Inscrição/index.js
+++ Aula 02 - Criando Lógica de Inscrição/index.js
@@ -93,7 +93,7 @@ const getTotalSubscribers = (userData)=>{
 
 
 const showInvite =(userData)=>{
-    app.innerHTML = `
+   /* app.innerHTML = `
         <input type="text" name="link" id="link" value="https://event.com?ref=${userData.ref}" disabled>
 
             <div id="status">
@@ -103,19 +103,104 @@ const showInvite =(userData)=>{
                 <p>
                     Inscrições Feitas
                 </p>
-            </div>`
+            </div>`*/
+
+
+            app.innerHTML = ` <main>
+                <h3>Inscrição confirmada</h3>
+                <p>Convide mais pessoas e concorra a prêmios!<br/> Compartilhe o link e acompanhe as inscrições:</p>
+
+                <div class="input-group">
+                    <label for="link">
+                        <img src="link.svg" alt="Link icon">
+                    </label>
+
+                    <input type="text" name="link" id="link" value="https://event.com?ref=${userData.ref}" disabled>
+                </div>
+            </main>
+
+
+            <section class="status">
+                    <h4>
+                    ${getTotalSubscribers(userData)}
+                </h4>
+                <p>Inscrições feitas</p>
+            </section>`
+
+            app.setAttribute('class', 'page-invite')
+            updateImageLinks()
+
 }
 
-const startApp = ()=>{
-    app.innerHTML = ` <form id="form">
-                             <input type="email" name="email" id="email" placeholder="Email">
-                             <input type="text" name="telefone" id="telefone" placeholder="Phone">
-                             <button id="btn-enviar">Enviar</button>
-                        </form>   `
+/*const updateImageLinks = ()=>{
 
+    document.querySelectorAll('img').forEach((img)=>{
+            img.src = `https://raw.githubusercontent.com/maykbrito/my-public-files/main/nlw-19/${img.src}`
+    })
+
+}*/
+
+
+
+  const updateImageLinks = () => {
+    document.querySelectorAll('img').forEach((img) => {
+      const src = img.getAttribute("src"); 
+      if (src && !src.startsWith("http")) {  
+        img.src = `https://raw.githubusercontent.com/maykbrito/my-public-files/main/nlw-19/${src}`;
+      }
+    });
+  };
+  
+
+const startApp = ()=>{
+    app.innerHTML = ` 
+            <main>
+                <section class="about">
+                    <div class="section-header">
+                        <h2>
+                            Sobre o evento
+                        </h2>
+                        <span class="badge">AO VIVO</span>
+                    </div>
+
+                    <p>Um evento feito por e para pessoas desenvolvedoras apaixonadas por criar soluções inovadoras e compartilhar conhecimento. Vamos mergulhar nas tendências mais recentes em desenvolvimento de software, arquitetura de sistemas e tecnologias emergentes, com palestras, workshops e hackathons.
+                        <br/><br/>Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito </p>
+                </section>
+
+                <section class="registration">
+                    <h2>Inscrição</h2>
+                    <form id="form">
+                        <div class="input-wrapper">
+                            <div class="input-group">
+                                <label for="email">
+                                    <img src="mail.svg" alt="Email icon">
+                                </label>
+                                <input type="email" name="email" id="email" placeholder="E-mail">
+                            </div>
+
+                            <div class="input-group">
+                                <label for="phone">
+                                    <img src="phone.svg" alt="Phone icon">
+                                </label>
+                                <input type="text" name="phone" id="phone" placeholder="Telefone">
+                            </div>
+                        </div>
+
+
+                        <button>Confirmar
+                            <img src="arrow.svg" alt="Arrow right">
+                        </button>
+                    </form>
+                </section>
+            </main> `
+    
+    app.setAttribute('class', 'page-start')
+
+    updateImageLinks()
     formAction()
 }
 
 startApp()
 
-document.getElementById("logo").onclick = () => startApp()
+document.querySelector("header").onclick = () => startApp()
+//document.getElementById("logo").onclick = () => startApp()
